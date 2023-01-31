8e568929a0a61fdc01261e6aca2c35f9645f839d
Joshinaidu733 <113463935+Joshinaidu733@users.noreply.github.com>
2023-01-31 18:27:28 +0530

    Add files via upload

---
+++ large_3_num.py
@@ -0,0 +1,9 @@
+a1=int(input("enter first number"))
+a2=int(input("enter second number"))
+a3=int(input("enter third number"))
+if(a1>a2 &a1>a3):
+    print(a1, "is greater")
+elif(a2>a3):
+    print(a2, "is greater")
+else:
+    print(a3, "is greater")
